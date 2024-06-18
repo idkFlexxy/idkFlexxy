1a218d6e8eb7fc703fdfe5cae1adc1c84fb0ec34
idkFlexxy <143282143+idkFlexxy@users.noreply.github.com>
2024-06-18 07:52:40 +0530

    P3

---
+++ Prog3.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+class Prog3 
+{
+ public static void main (String args[]) 
+{
+ int num1 = 15, num2 = 0, result = 0;
+try{
+ result = num1/num2;
+ System.out.println("The result is" +result);
+ } 
+ catch (ArithmeticException e) {
+ System.out.println ("Can't be divided by Zero " + e);
+ }
+ }
+}
